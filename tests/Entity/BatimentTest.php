f0744fe47ce0ba1a6c6fa54381c60c6aaee7f0cc
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-01-27 17:10:27 +0100

    correction du test de batiment

--- tests/Entity/BatimentTest.php
+++ tests/Entity/BatimentTest.php
@@ -11,11 +11,12 @@ class BatimentTest extends TestCase
     public function testBatiment(): void
     {
         $batiment = new Batiment();
+        $person = new Person("John", "Doe", "5 rue de la paix");
         $batiment->setAdress('5 rue de la paix');
-        $batiment->addPerson(new Person());
+        $batiment->addPerson($person);
         $this->assertInstanceOf(Person::class, $batiment->getPerson()->first());
-        $batiment->removePerson(new Person());
-        $this->assertEmpty($batiment->getPerson());
+        $batiment->removePerson($person);
+        $this->assertEquals(0, $batiment->getPerson()->count());
         $this->assertEquals('5 rue de la paix', $batiment->getAdress());
     }
 }
